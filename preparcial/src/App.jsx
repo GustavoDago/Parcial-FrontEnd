794d65ed9cb18c6f9f3faf3212c9332d150564ed
GustavoDago <gustavodagoberto@gmail.com>
2023-03-07 16:52:49 -0300

    Pusheo parcial Gustavo Dagoberto

--- preparcial/src/App.jsx
+++ preparcial/src/App.jsx
@@ -1,15 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Card from './components/Card'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [grupo, setGrupo] = useState("");
+  const [album, setAlbum] = useState("");
+  const [single, setSingle] = useState('')
+
+  const onChangeGrupo = (e) => setGrupo(e.target.value);
+  const onChangeAlbum = (e) => setAlbum(e.target.value);
+  const onChangeSingle = (e) => setSingle(e.target.value)
+
+  const manejador = (event) => {
+    event.preventDefault()
+    if (grupo.trim().length > 2 && album.length > 5) {
+      setShow(true)
+      setErr(false)
+    } else {
+      setErr(true)
+    }
+  }
+
+  const [show, setShow] = useState(false)
+  const [err, setErr] = useState(false)
 
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Tecno de los '90</h1>
+      <form onSubmit={manejador}>
+        <div>
+          <label>Ingrese nombre del grupo</label>
+          <input type="text" placeholder='ingrese nombre del grupo' value={grupo} onChange={onChangeGrupo} />
+          <label>Mejor Disco</label>
+          <input type="text" value={album} onChange={onChangeAlbum} />
+          <label>Mejor Single</label>
+          <input type="text" value={single} onChange={onChangeSingle} />
+        </div>
+        <button type='submit'>Enviar</button>
+        {err && 'Por favor chequea que la información sea correcta'}
+      </form>
+      {show && <Card nombreGrupo={grupo} mejorDisco={album} mejorSingle={single} />}
     </div>
   )
 }
--- preparcial/src/components/Card.jsx
+++ preparcial/src/components/Card.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({nombreGrupo, mejorDisco, mejorSingle}) => {
+  return (
+    <div>
+        <h3>Grupo: {nombreGrupo}</h3>
+        <h3>Mejor disco: {mejorDisco}</h3>
+        <h3>Mejor single: {mejorSingle} </h3>
+    </div>
+  )  
 };
 export default Card;
